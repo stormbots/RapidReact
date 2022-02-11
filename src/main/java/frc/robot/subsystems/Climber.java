c38a2910da1cd7019c7001d8e89eb952c618ae75
Michael Wilde <techbub619@gmail.com>
2022-02-10 20:51:04 -0800

    Successful climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -12,6 +12,8 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.stormbots.Clamp;
 import com.stormbots.closedloop.MiniPID;
@@ -25,8 +27,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
   //Class consents including the min height and max height
-  private double kMinHeight = 6;
-  private double kMaxHeight = 20;
+  private double kMinHeight = 0;
+  private double kMaxHeight = 18.5;
 
   //Class consent variables for the motor for the hookMotor and climber, also gets the encoder for the hook and the PID controller
   private CANSparkMax hookMotor = new CANSparkMax(17, MotorType.kBrushless); //Change the port later, 10 is temp variable
@@ -37,6 +39,9 @@ public class Climber extends SubsystemBase {
   private RelativeEncoder winchEncoder = winchMotor.getEncoder();
   MiniPID winchPID = new MiniPID(0,0,0);//TODO: change temp values. 
 
+  // SlewRateLimiter
+  SlewRateLimiter winchSlewRate = new SlewRateLimiter(1); //TO-DO; change the name later and temp value of 1\
+  
   //Note to self; motorHook switched to hookMotor and motorCLimber switched to winchMotor
 
   //Switch imports
@@ -46,23 +51,33 @@ public class Climber extends SubsystemBase {
 
   /** Creates a new Climber. */
   public Climber() {
-    //configure hook motors/encoder
-    hookMotor.setSmartCurrentLimit(2);//Note: 2 is temp variable
     //Get the position of the encoder
     hookEncoder.getPosition();
 
     //configure climber hook/motors
       //configure conversion factor for encoder so that X rotations  = linear distance
       //configure offset for that value
-      winchEncoder.setPositionConversionFactor(1); //TODO set conversion to inches from floor;
-      winchEncoder.setPosition(0); //TODO Set to resting height from floor
+      winchMotor.setInverted(true);
+      winchEncoder.setPositionConversionFactor((kMaxHeight-kMinHeight)/136.25); //TODO set conversion to inches from floor;
+      winchEncoder.setPosition(kMinHeight); //TODO Set to resting height from floor
+      
+      winchMotor.setSoftLimit(SoftLimitDirection.kForward, (float)kMaxHeight);
+      winchMotor.setSoftLimit(SoftLimitDirection.kReverse, (float)kMinHeight);
+      winchMotor.enableSoftLimit(SoftLimitDirection.kForward, true);
+      winchMotor.enableSoftLimit(SoftLimitDirection.kReverse, true);
+
       winchPID.setSetpoint(winchEncoder.getPosition());
+      winchMotor.setIdleMode(IdleMode.kCoast);//Want coast on boot, enable brake when climbing
 
       //Configure Hook
-      hookMotor.setSmartCurrentLimit(5);
-      hookEncoder.setPositionConversionFactor(1); //TODO set conversion to degres/angle
+      hookMotor.setSmartCurrentLimit(2);
+      hookMotor.setInverted(true);
+      hookEncoder.setPositionConversionFactor(180/2.3); //TODO set conversion to degres/angle
+      hookEncoder.setPosition(210);
       hookPID.setReference(hookEncoder.getPosition(), ControlType.kPosition);
-      hookPID.setP(0);
+      hookPID.setP(1/360.0);
+      hookMotor.setIdleMode(IdleMode.kCoast);
+
   } 
 
   // getHeight()
@@ -90,10 +105,15 @@ public class Climber extends SubsystemBase {
     // power *=0.3;
     winchMotor.set(power);
   }
-  
 
-  // SlewRateLimiter
-  SlewRateLimiter winchSlewRate = new SlewRateLimiter(1); //TO-DO; change the name later and temp value of 1\
+  
+  public void init(){
+    winchMotor.setIdleMode(IdleMode.kBrake);
+  }
+
+  public void disabledInit(){
+    winchMotor.setIdleMode(IdleMode.kCoast);
+  }
 
   @Override
   public void periodic() {
@@ -102,7 +122,7 @@ public class Climber extends SubsystemBase {
     // winchMotor.set(power);
 
     // set a pid config and setup for the hook
-
+    // double hookMotorPower = hookPID.setReference(value, ControlType.kPosition);
     
     // print target, current values, and outputs for all pid systems
     // Ex;SmartDashboard.putString("climber/whatever", value);

f8af3b552a40a4ec9b7034e9f9819d2cbde8dd19
Jesse <jwolfwa@gmail.com>
2022-02-24 20:12:06 -0800

    Took out ShooterRpmDistance parameters

--- src/main/java/frc/robot/commands/ShooterSpoolUp.java
+++ src/main/java/frc/robot/commands/ShooterSpoolUp.java
@@ -27,7 +27,7 @@ public class ShooterSpoolUp extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    shooter.shooterSpoolUpToSpeed();
+    shooter.setRPMForDistance();
   }
 
   // Called once the command ends or is interrupted.
